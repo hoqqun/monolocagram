95f63345ad56c2aaa334b0ca1da09618d595c4e8
Hokuto Shimura <hoqqun@gmail.com>
2017-06-19 04:47:09 +0000

    デプロイ先変更

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 lock '3.6.0'
 
 # デプロイするアプリケーション名
-set :application, 'achieve'
+set :application, 'monolocagram'
 
 # cloneするgitのレポジトリ（xxxxxxxx：ユーザ名、yyyyyyyy：アプリケーション名）
 set :repo_url, 'https://github.com/hoqqun/monolocagram'
@@ -10,7 +10,7 @@ set :repo_url, 'https://github.com/hoqqun/monolocagram'
 set :branch, ENV['BRANCH'] || 'master'
 
 # deploy先のディレクトリ。
-set :deploy_to, '/var/www/achieve'
+set :deploy_to, '/var/www/monolocagram'
 
 # シンボリックリンクをはるフォルダ・ファイル
 set :linked_files, %w{.env config/secrets.yml}
